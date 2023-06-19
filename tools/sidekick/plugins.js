8c608dc8582768743db0bd5f83ff0e9fc7ddcc11
paraschi <paraschi@adobe.com>
2023-06-19 11:45:58 +0300

    configure acc sidekick

--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -65,62 +65,91 @@ class EmailSidekickBanner {
 }
 
 const copyHtml = () => {
-    const iframe = document.getElementById('__emailFrame');
-    if (iframe) {
-        navigator.clipboard.writeText(iframe.srcdoc).then(() => {
-            const banner = new EmailSidekickBanner('get-html-data');
-            banner.write('HTML copied to clipboard', 5);
-        });
-    }
+  const iframe = document.getElementById('__emailFrame');
+  if (iframe) {
+    navigator.clipboard.writeText(iframe.srcdoc).then(() => {
+    });
+  }
 };
+
+const sendHtml = () =>
+{
+  const html = copyHtml();
+  const url = 'https://campeng-mkt-prod1.campaign.adobe.com/nms/amcCreateDelivery.jssp';
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `template=mail&htmlContent=${html}`,
+    credentials: 'include'
+  };
+
+  window.fetch(url, requestOptions)
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error('Request failed with status: ' + response.status);
+        }
+      })
+      .then(data => {
+        console.log(data); // Process the response data here
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+}
 
 const downloadHtml = () => {
   const iframe = document.getElementById('__emailFrame');
-    if (iframe) {
-      const h1 = iframe.contentWindow.document.body.querySelector('h1');
-      const title = h1 ? h1.innerText : 'New E-Mail';
-      const subject = `Subject: ${title}`;
-      const to = 'To: noreply@adobe.com';
-      const html = iframe.srcdoc;
-      const fileName = title
-        .replaceAll(/\W+/g, '-')
-        .replaceAll(/[-]{2,}/g, '-')
-        .toLowerCase()
+  if (iframe) {
+    const h1 = iframe.contentWindow.document.body.querySelector('h1');
+    const title = h1 ? h1.innerText : 'New E-Mail';
+    const subject = `Subject: ${title}`;
+    const to = 'To: noreply@adobe.com';
+    const html = iframe.srcdoc;
+    const fileName = title
+            .replaceAll(/\W+/g, '-')
+            .replaceAll(/[-]{2,}/g, '-')
+            .toLowerCase()
         + '.emltpl'
 
-      const eml = to + '\n' 
+    const eml = to + '\n'
         + subject + '\n'
         + 'Content-Type: text/html;charset=utf-8\n'
         + 'X-Unsent: 1'+'\n'
         + '\n'
         + '\n'
         + html;
-      const bytes = new TextEncoder().encode(eml);
-      const blob = new Blob([bytes], { type: 'text/plain;charset=utf-8' });
-      const url = URL.createObjectURL(blob);
+    const bytes = new TextEncoder().encode(eml);
+    const blob = new Blob([bytes], { type: 'text/plain;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
 
-      const fileLink = document.createElement('a');
-      var linkText = document.createTextNode(fileName);
-      fileLink.appendChild(linkText);
-      fileLink.href = url;
-      fileLink.download = fileName;
-      fileLink.style.visibility = 'hidden';
-      
-      fileLink.onclick = () => {
-        setTimeout(() => {
-          fileLink.remove();
-          URL.revokeObjectURL(url);
-        })
-      }
-      
-      document.body.appendChild(fileLink);
-      fileLink.click();
+    const fileLink = document.createElement('a');
+    let linkText = document.createTextNode(fileName);
+    fileLink.appendChild(linkText);
+    fileLink.href = url;
+    fileLink.download = fileName;
+    fileLink.style.visibility = 'hidden';
+
+    fileLink.onclick = () => {
+      setTimeout(() => {
+        fileLink.remove();
+        URL.revokeObjectURL(url);
+      })
     }
+
+    document.body.appendChild(fileLink);
+    fileLink.click();
+  }
 }
 
 const sk = document.querySelector('helix-sidekick');
 sk.addEventListener('custom:copyHtml', copyHtml);
 sk.addEventListener('custom:downloadHtml', downloadHtml);
+sk.addEventListener('custom:sendHtml', sendHtml);
 
 window.addEventListener('message', ({ data, origin, source }) => {
   if (data === 'mjml2html' && origin.match('localhost(:\\d+)?$|.*\\.hlx\\.(page|live)$')) {
